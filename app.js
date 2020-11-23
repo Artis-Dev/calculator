75b013e7c9290a9dea4210eab759c6660c988538
Artis-Dev <admin@moonart.lt>
2020-11-23 13:24:07 +0200

    Create calculator math functions

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
 console.log('Hello World!');
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b
+}
+
+function sum(arr) {
+  let value = 0;
+  for(let i = 0; i < arr.length; i++) {
+    value += arr[i];
+  }
+  return value;
+}
+
+function multiply(arr) {
+  let value = 1;
+  for(let i = 0; i < arr.length; i++) {
+    value *= arr[i];
+  }
+  return value;
+}
+
+function power(a, b) {
+  let value = a;
+  for(let i = 1; i < b; i++) {
+    value *= a;
+  }
+  return value;
+}
+
+function factorial(a) {
+  let value = 1;
+  for(let i =  1; i <= a; i++) {
+    value *= i;
+  }
+  return value;
+}
+
+function operate(operator, a, b) {
+
+}
